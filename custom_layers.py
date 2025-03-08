Fix: masking and tuning the acvation range
Make the train AUC score improve a lot

custom_layers.py
@@ -1,39 +1,58 @@
 import tensorflow as tf
-
+from tensorflow.keras.initializers import HeNormal
+import matplotlib.pyplot as plt
+import numpy as np
+from tensorflow.keras.callbacks import Callback
+import csv
 class StaggeredConv1D(tf.keras.layers.Layer):
-    """Layers which use two CNNs to scan alternately."""
+    """Layers which use two CNNs to scan alternately and provide intermediate outputs for visualization."""
     
     def __init__(self, filters=32, kernel_size=3, **kwargs):
         """Initialize underlying layers."""
         super().__init__()
         self.filters = filters
         self.kernel_size = kernel_size
-        self.conv1d_a = tf.keras.layers.Conv1D(filters, kernel_size, padding='same', **kwargs)
-        self.conv1d_b = tf.keras.layers.Conv1D(filters, kernel_size, padding='same', **kwargs)
+        self.supports_masking = True
+        self.conv1d_a = tf.keras.layers.Conv1D(
+            self.filters, 
+            self.kernel_size, 
+            padding='same', 
+            **kwargs
+        )
 
+        self.conv1d_b = tf.keras.layers.Conv1D(
+            self.filters, 
+            self.kernel_size, 
+            padding='same', 
+            **kwargs
+        )
+    
     def call(self, inputs, training=None, mask=None):
-        """Forward pass."""
-
+        """Forward pass with optional intermediate output for visualization."""
         if mask is not None:
-            inputs = tf.identity(inputs)
+            mask = tf.cast(mask, tf.float32)  
+            mask = tf.expand_dims(mask, axis=-1)  
+            inputs = inputs * mask  
 
-        conv_a = self.conv1d_a(inputs)[:, ::2, :]
-        conv_b = self.conv1d_b(inputs)[:, 1::2, :]
+        inputs_a = inputs[:, ::2, :] 
+        inputs_b = inputs[:, 1::2, :]  
+        
+        conv_a = self.conv1d_a(inputs_a) 
+        conv_b = self.conv1d_b(inputs_b) 
 
-        min_length = tf.minimum(tf.shape(conv_a)[1], tf.shape(conv_b)[1])
-        conv_a = tf.cond(tf.shape(conv_a)[1] > min_length, lambda: conv_a[:, :min_length, :], lambda: conv_a)
-        conv_b = tf.cond(tf.shape(conv_b)[1] > min_length, lambda: conv_b[:, :min_length, :], lambda: conv_b)
+        max_length = tf.maximum(tf.shape(conv_a)[1], tf.shape(conv_b)[1])
 
-        staggered = tf.reshape(tf.stack([conv_a, conv_b], axis=-2), 
-                            (tf.shape(inputs)[0], tf.shape(inputs)[1], self.filters))
+        padded_conv_a = tf.pad(conv_a, [[0, 0], [0, max_length - tf.shape(conv_a)[1]], [0, 0]])
+        padded_conv_b = tf.pad(conv_b, [[0, 0], [0, max_length - tf.shape(conv_b)[1]], [0, 0]])
 
+        staggered = tf.reshape(tf.stack([padded_conv_a, padded_conv_b], axis=-2), 
+                            (tf.shape(inputs)[0], max_length * 2, self.filters))
+
+        
         return tf.cast(staggered, tf.float32)
 
     def compute_output_shape(self, input_shape):
         return (input_shape[0], input_shape[1], self.filters)  
-
-    def compute_mask(self, inputs, mask=None):
-        return None  
 
     def get_config(self):
         config = super().get_config()
@@ -42,3 +61,24 @@ class StaggeredConv1D(tf.keras.layers.Layer):
             "kernel_size": self.kernel_size,
         })
         return config
+    
+
+# def save_to_csvb(self, x):
+#         """Save tensor to CSV file."""
+#         filename="inputInCNNConB.csv"
+#         try:
+#             np_array = x.numpy()  # 轉為 numpy array
+#             print(f"Saving to {filename}, shape:", np_array.shape)
+#             with open(filename, mode='w', newline='') as file:
+#                 writer = csv.writer(file)
+#                 for sample in np_array:
+#                     for row in sample:
+#                         writer.writerow(row)
+#         except Exception as e:
+#             print(f"Error saving to {filename}:", e)
+#         return x
+
+# tmp = staggered
+#         tmp = tf.keras.layers.Lambda(
+#             lambda x: tf.py_function(self.save_to_csvfinal, [x], tf.float32)
+#         )(tmp)

rally_classifier.py
@@ -10,7 +10,10 @@ from keras_transformer.gelu import gelu
 from tensorflow.keras.layers import MultiHeadAttention, Dense, LayerNormalization, Dropout, Embedding
 from custom_layers import StaggeredConv1D
 import tensorflow.keras.activations as activations
-
+import numpy as np
+from tensorflow.keras.layers import LeakyReLU
+from tensorflow.keras.regularizers import l2
+import csv
 def preprocess_inputs(shot_sequence_shape, rally_info_shape,
                       embed_types_size=None, embed_area_size=None, embed_player_size=None):
     """Preprocess input encoding: Input + Embedding + Concatenation + Masking"""
@@ -23,15 +26,16 @@ def preprocess_inputs(shot_sequence_shape, rally_info_shape,
     input_opponent_area = tf.keras.Input(shape=(shot_sequence_shape[0],), name='Opponent_area_input')
     input_player_id = tf.keras.Input(shape=(shot_sequence_shape[0],), name='Player_input')
     input_rally = tf.keras.Input(shape=(rally_info_shape,), name='Rally_input')
+    input_masks = tf.keras.Input(shape=(shot_sequence_shape[0],), name='Mask_input')
 
     # ✅ Player Embedding
     if embed_player_size is not None:
-        player_embedding = tf.keras.layers.Embedding(input_dim=embed_player_size, output_dim=15, mask_zero=True, name='Player_embedding')
+        player_embedding = tf.keras.layers.Embedding(input_dim=embed_player_size, output_dim=15, mask_zero=True, name='Player_embedding',embeddings_initializer=tf.keras.initializers.RandomUniform(minval=-1, maxval=1))
         embedded_player = player_embedding(input_player_id)  # (None, 66, 15)
 
     # ✅ Location Embedding (Hit Area, Player Area, Opponent Area)
     if embed_area_size is not None:
-        area_embedding = tf.keras.layers.Embedding(input_dim=embed_area_size, output_dim=10, mask_zero=True, name='Area_embedding')
+        area_embedding = tf.keras.layers.Embedding(input_dim=embed_area_size, output_dim=10, mask_zero=True, name='Area_embedding',embeddings_initializer=tf.keras.initializers.RandomUniform(minval=-1, maxval=1))
         embedded_hit_area = area_embedding(input_hit_area)
         embedded_player_area = area_embedding(input_player_area)
         embedded_opponent_area = area_embedding(input_opponent_area)
@@ -40,37 +44,41 @@ def preprocess_inputs(shot_sequence_shape, rally_info_shape,
 
     # ✅ Shot Type Embedding with Time Influence
     if embed_types_size is not None:
-        shot_embedding = tf.keras.layers.Embedding(input_dim=embed_types_size, output_dim=15, mask_zero=True, name='Shot_types_embedding')
+        shot_embedding = tf.keras.layers.Embedding(input_dim=embed_types_size, output_dim=15, mask_zero=True, name='Shot_types_embedding',embeddings_initializer=tf.keras.initializers.RandomUniform(minval=-1, maxval=1))
         embedded_shot_types = shot_embedding(input_shot_types)
 
-        shot_mu_embedding = tf.keras.layers.Embedding(input_dim=embed_types_size, output_dim=15, mask_zero=True, name='Time_influence_occurrence')
-        shot_theta_embedding = tf.keras.layers.Embedding(input_dim=embed_types_size, output_dim=15, mask_zero=True, name='Time_influence_shot')
+        mu_n = Dense(15, activation='linear', 
+             kernel_initializer=tf.keras.initializers.RandomUniform(minval=-1, maxval=1), 
+             name='Mu_latent')(embedded_shot_types)
+        theta_n = Dense(15, activation='linear', 
+                        kernel_initializer=tf.keras.initializers.RandomUniform(minval=-1, maxval=1), 
+                        name='Theta_latent')(embedded_shot_types)
 
         # ✅ Time Proportion Enhancement
-        tiled_time_proportion = tf.keras.layers.Lambda(lambda x: tf.expand_dims(x, axis=-1))(input_time_proportion)
+        tiled_time_proportion = tf.keras.layers.Lambda(
+            lambda x: tf.expand_dims(x, axis=-1), 
+            output_shape=(62, 1)
+        )(input_time_proportion)
         tiled_time_proportion = tf.keras.layers.Lambda(lambda x: tf.tile(x, [1, 1, 15]))(tiled_time_proportion)
 
         # μn * τn
-        time_mu_proportion = tf.keras.layers.Multiply(name='Time_proportion_multiply')([shot_mu_embedding(input_shot_types), tiled_time_proportion]) 
+        time_mu_proportion = tf.keras.layers.Multiply(name='Time_proportion_multiply')([mu_n, tiled_time_proportion])
         # θn + μn * τn
-        temporal_score = tf.keras.layers.Add(name='Time_proportion_add')([shot_theta_embedding(input_shot_types), time_mu_proportion])
+        temporal_score = tf.keras.layers.Add(name='Time_proportion_add')([theta_n, time_mu_proportion])
         # δn = sigmoid(θn + μn * τn)
         temporal_score = tf.keras.layers.Activation('sigmoid', name='Time_activation')(temporal_score)
 
         enhanced_shot_features = tf.keras.layers.Multiply(name='Shots_time_multiply')([temporal_score, embedded_shot_types])
     else:
-        enhanced_shot_features = input_shots
-
+        enhanced_shot_features = input_shot_types
+ 
     # ✅ **Concatenate Player Embedding, Location Embedding, and Enhanced Shot Features (Shot Encoder Output)**
     shot_encoder_output = tf.keras.layers.Concatenate(name='Shot_encoder_output')([
         embedded_hit_area, embedded_player_area, embedded_opponent_area, enhanced_shot_features, embedded_player,input_shots
     ])
-    # ✅ Masking Layer
-    layer_masking = tf.keras.layers.Masking(name='Sequence_masking')
-    masked_sequence = layer_masking(shot_encoder_output)
 
     return [input_shots,input_shot_types,input_player_id,input_time_proportion, input_hit_area, input_player_area, input_opponent_area, 
-              input_rally], masked_sequence
+              input_rally,input_masks], shot_encoder_output
 
 
 # Proposed modal: CNN + Position + Mutihead Attention
@@ -84,14 +92,15 @@ def proposed_model(shot_sequence_shape: Tuple[int, int],
                    dense_kwargs: Dict[str, Any] = {}) -> tf.keras.Model:
 
     # ✅ Get Processed Inputs and Shot Encoder Output
-    inputs, masked_sequence = preprocess_inputs(
+    inputs, shot_encoder_output = preprocess_inputs(
         shot_sequence_shape, rally_info_shape,
         embed_types_size=embed_types_size, embed_area_size=embed_area_size, embed_player_size=embed_player_size
     )
 
-    # ✅ CNN Feature Extraction
+    # ✅ CNN Feature Extraction with Masking
     layer_cnn = StaggeredConv1D(name='Local_pattern_extraction', **cnn_kwargs)
-    pattern_sequence = layer_cnn(masked_sequence)
+    pattern_sequence = layer_cnn(shot_encoder_output, mask=inputs[-1])
+    pattern_sequence = tf.keras.layers.Dropout(0.3)(pattern_sequence) 
 
     # ✅ Positional Encoding
     seq_len = shot_sequence_shape[0]
@@ -99,28 +108,49 @@ def proposed_model(shot_sequence_shape: Tuple[int, int],
     pattern_sequence_with_pos = pattern_sequence + pos_encoding
 
     # ✅ Transformer Encoder
-    mha = MultiHeadAttention(num_heads=transformer_kwargs['num_heads'], key_dim=transformer_kwargs['key_dim'])
-    attn_output, attn_weights = mha(pattern_sequence_with_pos, pattern_sequence_with_pos, return_attention_scores=True)
+    num_heads = transformer_kwargs['num_heads']
+    seq_len = pattern_sequence_with_pos.shape[1]
+
+    #  (batch_size, 1, seq_len)
+    mask = tf.keras.layers.Lambda(lambda x: tf.expand_dims(x, axis=1), 
+        output_shape=lambda s: (s[0], 1, s[1]), 
+        name='Expand_mask_dim_1'
+    )(inputs[-1])
+
+    # (batch_size, 1, 1, seq_len)
+    mask = tf.keras.layers.Lambda(lambda x: tf.expand_dims(x, axis=1), 
+        output_shape=lambda s: (s[0], 1, 1, s[2]), 
+        name='Expand_mask_dim_2'
+    )(mask)
+
+    # Tile to (batch_size, num_heads, seq_len, seq_len)
+    mask = tf.keras.layers.Lambda(lambda x: tf.tile(x, [1, num_heads, 1, 1]), 
+        output_shape=lambda s: (s[0], num_heads, s[2], s[3]), 
+        name='Tile_mask_for_heads'
+    )(mask)
+
+    mha = MultiHeadAttention(num_heads=num_heads, key_dim=transformer_kwargs['key_dim'])
+    attn_output, attn_weights = mha(
+        pattern_sequence_with_pos, 
+        pattern_sequence_with_pos, 
+        attention_mask=mask, 
+        return_attention_scores=True
+    )
     attn_output = LayerNormalization(epsilon=1e-6)(attn_output + pattern_sequence_with_pos)
 
     # ✅ Feed Forward Network
-    ffn = Dense(transformer_kwargs['inner_dim'], activation=activations.gelu)(attn_output)  # Expand to `inner_dim`
-    ffn = Dropout(0.5)(ffn)
-    ffn_output = Dense(transformer_kwargs['ff_dim'])(ffn)  # Reduce back to `ff_dim`
-    ffn_output = Dropout(0.5)(ffn_output)
+    ffn = Dense(transformer_kwargs['inner_dim'], activation=activations.gelu)(attn_output)  
+    ffn_output = Dense(transformer_kwargs['ff_dim'])(ffn)  
     transformer_output = LayerNormalization(epsilon=1e-6)(ffn_output + attn_output)
 
     # ✅ Max Pooling
     rally_representation = tf.keras.layers.GlobalMaxPooling1D()(transformer_output)
 
     # ✅ Concatenate with Rally Information
-    layer_concat_rally = tf.keras.layers.Concatenate(name='Seq_rally_merging')([rally_representation, inputs[-1]])
-
+    layer_concat_rally = tf.keras.layers.Concatenate(name='Seq_rally_merging')([rally_representation, inputs[-2]])
     # ✅ Final Dense Layer
-    fc_layer = Dense(16, activation='relu', kernel_regularizer=tf.keras.regularizers.l2(0.01))(layer_concat_rally)
-    fc_layer = Dropout(0.5)(fc_layer)
+    output_win_prob = Dense(1, activation='sigmoid', kernel_regularizer=l2(0.01))(layer_concat_rally)
 
-    output_win_prob = Dense(1, activation='sigmoid')(fc_layer)
     model_predict = tf.keras.Model(inputs=inputs, outputs=output_win_prob)
     return model_predict
 

train.py
@@ -33,24 +33,79 @@ def prepare_data(dataset: pd.DataFrame,
     """Convert dataset to appropriate format for training."""
     shots = []
     rallies = []
-    rally_ids = []
+    masks=[]
+
     shot_attributes_f = util.flatten(shot_attributes)
     rally_attributes_f = util.flatten(rally_attributes)
-    dataset_copy = dataset.copy()
 
-    # Generate sequences of rallies
-    for rally_id, rally in dataset_copy.groupby("rally_id"):
+    pre_setid = None
+    consecutive_points = 0
+    last_getpoint_player = None
+    for rally_id, rally in dataset.groupby('rally_id'):
         if min_len > 0 and len(rally) < min_len:
             continue
+        
+        # ====== (Time Proportion) ======
+        rally['time_proportion'] = np.linspace(0, 1, len(rally))
+
+        getpoint_player = rally['getpoint_player'].iloc[-1]
+        # ====== (Score Difference) ======
+        setid = rally['set'].iloc[-1]
+        score_A = rally['roundscore_A'].iloc[-1]
+        score_B = rally['roundscore_B'].iloc[-1]
+
+        if setid != pre_setid:
+            prev_score_A = 0
+            prev_score_B = 0
+            score_diff = 0
+        else:
+            if score_A > prev_score_A and score_B == prev_score_B: 
+                score_diff = score_A - score_B
+            elif score_B > prev_score_B and score_A == prev_score_A:  
+                score_diff = score_B - score_A
+            else:
+                score_diff = 0  
+
+        prev_score_A = score_A
+        prev_score_B = score_B
+        rally['roundscore_diff'] = score_diff
+            
+         # (Consecutive Points)
+        if setid != pre_setid: 
+            last_getpoint_player = None
+            consecutive_points = 1
+        else:
+            if getpoint_player == last_getpoint_player:
+                consecutive_points += 1
+            else:
+                consecutive_points = 1 
+
+        last_getpoint_player = getpoint_player
+        rally['consecutive_points'] = consecutive_points
+        pre_setid = setid
+
+        if 'time_proportion' not in shot_attributes_f:
+            shot_attributes_f.append('time_proportion')
+        if 'roundscore_diff' not in rally_attributes_f:
+            rally_attributes_f.append('roundscore_diff')
+        if 'consecutive_points' not in rally_attributes_f:
+            rally_attributes_f.append('consecutive_points')
+
         shots.append(rally[shot_attributes_f].values.astype('float32'))
-        rallies.append(rally[rally_attributes_f].values[-1].astype('float32')) 
-        rally_ids.append(rally["rally_id"].values[-1])
-        # Force non-target's sequence starts at second step
-        pad = ((0, pad_to - len(rally)) if rally['is_target_turn'].iloc[0]
-               else (1, pad_to - len(rally) - 1))
-        shots[-1] = np.pad(shots[-1], [pad, (0, 0)])
+        rally_features = rally[rally_attributes_f].values[-1].astype('float32')
+        rallies.append(rally_features)
+
+        # ====== (Padding) ======
+        pad = ((0, pad_to - len(rally)), (0, 0))
+        shots[-1] = np.pad(shots[-1], pad, mode='constant', constant_values=0)
+        # creating mask
+        # 對整個 batch 的數據生成遮罩
+        mask = np.any(shots[-1] != 0, axis=-1).astype(np.float32)
+        masks.append(mask)
+
     shots = np.asarray(shots)
     rallies = np.asarray(rallies)
+    masks = np.asarray(masks)
     # Split back to input specification
     shot_attributes_len = util.list_len(shot_attributes)
     if len(shot_attributes_len) > 1:
@@ -65,4 +120,4 @@ def prepare_data(dataset: pd.DataFrame,
         for i in range(len(rallies)):
             if rally_attributes_len[i] == 1:
                 rallies[i] = rallies[i][:, 0]
-    return (shots, rallies, rally_ids)
+    return (shots, rallies,masks)

training.py
@@ -8,61 +8,49 @@ import util
 import os
 import glob
 from sklearn.model_selection import train_test_split
+from tensorflow.keras.callbacks import Callback
+
+# Timestamp for model saving
 timestr = time.strftime("%Y%m%d-%H%M%S")
 
-dataset = pd.read_csv('new_data/dataset.csv')
+# Load data
+train_data = pd.read_csv('./data/train.csv')
+val_given_data = pd.read_csv('./data/val_given.csv')
+val_gt_data = pd.read_csv('./data/val_gt.csv')
 
-encode_columns = []
-shot_predictors = ['player_id','time_proportion', 'backhand', 'aroundhead','hit_area', 'player_location_area', 'opponent_location_area',"type"]
-rally_predictors = ['roundscore_diff', 'continuous_score']
+print(f"Train data shape: {train_data.shape}")
+print(f"Validation data shape: {val_given_data.shape}, GT: {val_gt_data.shape}")
+
+# ✅ Data preprocessing parameters
+shot_predictors = ['player', 'type', 'backhand', 'aroundhead', 
+                   'hit_area', 'player_location_area', 'opponent_location_area']
+rally_predictors = ['roundscore_diff', 'consecutive_points']  
 target = 'is_target_win'
 
 # ✅ Find largest seq len for padding
-seq_len = dataset.groupby("rally_id").size().max()
-seq_len += 1 if seq_len % 2 == 1 else 2
+seq_len = train_data.groupby('rally_id').size().max()
+seq_len += 1 if seq_len % 2 == 1 else 2 
 
-# ✅ One-Hot Encoding
-encoded = pd.get_dummies(dataset, columns=encode_columns)
-codes_type, uniques_type = pd.factorize(encoded['type'])
-encoded['type'] = codes_type + 1  # Reserve 0 for padding
+# ✅ Encode 'type' column
+codes_type, uniques_type = pd.factorize(train_data['type'])
+train_data['type'] = codes_type + 1  
+val_given_data['type'] = val_given_data['type'].apply(
+    lambda x: (uniques_type.tolist().index(x) + 1) if x in uniques_type else 0
+)
 
-shot_predictors = [c for c in encoded.columns if any(c.startswith(f'{p}_') for p in shot_predictors) or c in shot_predictors]
-
-# 80/10/10
-unique_rallies = encoded["rally_id"].drop_duplicates()
-
-train_rally, val_test_rally = train_test_split(unique_rallies, test_size=0.2, random_state=42)
-
-val_rally, test_rally = train_test_split(val_test_rally, test_size=0.5, random_state=42)
-
-print(f"📊 Train Rally: {len(train_rally)}, Val Rally: {len(val_rally)}, Test Rally: {len(test_rally)}")
-
-train_data = encoded.merge(train_rally, on= "rally_id")
-val_data = encoded.merge(val_rally, on="rally_id")
-test_data = encoded.merge(test_rally, on= "rally_id")
-
-print(f"📊 Train: {len(train_data)}, Val: {len(val_data)}, Test: {len(test_data)}")
-
-# ✅ save data
-test_data.to_csv("./split_data/test_data.csv", index=False)
-train_data.to_csv("./split_data/train_data.csv", index=False)
-val_data.to_csv("./split_data/val_data.csv", index=False)
-print("✅ dataset saved successfully!")
-
-(train_shots), (train_rallies, train_target),train_rally_id = train.prepare_data(
+(train_shots), (train_rallies, train_target,train_rally_id)= train.prepare_data(
     train_data, 
     [shot_predictors], 
-    [rally_predictors, target],  
+    [rally_predictors,target,'rally_id'],
     pad_to=seq_len
 )
 
-(val_shots), (val_rallies, val_target),val_rally_id = train.prepare_data(
-    val_data, 
+(val_shots), (val_rallies, val_target,val_rally_id) = train.prepare_data(
+    val_given_data, 
     [shot_predictors], 
-    [rally_predictors, target],  
+    [rally_predictors,target,'rally_id'],
     pad_to=seq_len
 )
-
 seq_len = train_shots.shape[1]
 
 train_player_id = train_shots[:, :, 0].copy()
@@ -84,10 +72,10 @@ indices_to_delete = [0, 1, 4, 5, 6, 7]
 val_shots = np.delete(val_shots, indices_to_delete, axis=2)
 
 # ✅ Set model hyperparameters
-regularizer = tf.keras.regularizers.l2(0.1)
+regularizer = tf.keras.regularizers.l2(0.01)
 loss = 'binary_crossentropy'
 metrics = ['AUC', 'binary_accuracy']
-epochs = 100
+epochs = 20
 
 MODEL_DIR = "./model/"
 MODEL_NAME = "proposedModal"  
@@ -95,8 +83,9 @@ MODEL_PATH = os.path.join(MODEL_DIR, MODEL_NAME)
 os.makedirs(MODEL_PATH, exist_ok=True)
 
 n_shot_types = len(uniques_type) + 1
-n_area_types = encoded['player_location_area'].nunique() + 1
-n_player_types = encoded['player_id'].nunique() + 1  
+n_area_types = max(train_data['player_location_area'].nunique(), 
+                   train_data['opponent_location_area'].nunique()) + 1  
+n_player_types = train_data['player'].nunique() + 1  
 cnn_kwargs = {'filters': 32, 'kernel_size': 3, 'kernel_regularizer': regularizer, 'activation': 'relu'}
 transformer_kwargs = {
     'num_heads': 1,  # Reduce heads to match paper
@@ -106,7 +95,7 @@ transformer_kwargs = {
 }
 dense_kwargs = {'kernel_regularizer': regularizer}
 batch_size = 32
-optimizer = tf.keras.optimizers.Adam(learning_rate=0.001)  
+optimizer = tf.keras.optimizers.Adam(learning_rate=1e-3, clipnorm=1.0)  
 
 import json
 param_dict = {
@@ -155,7 +144,47 @@ checkpoint_callback = tf.keras.callbacks.ModelCheckpoint(
     verbose=1
 )
 
-callbacks = tf.keras.callbacks.EarlyStopping(min_delta=0.001, patience=15, restore_best_weights=True),
+class EpochDebugCallback(Callback):
+    def __init__(self, train_data, train_labels, val_data, val_labels, batch_size=32):
+        super().__init__()
+        self.train_data = train_data
+        self.train_labels = tf.reshape(train_labels, (-1, 1))  # 調整標籤數據形狀
+        self.val_data = val_data
+        self.val_labels = tf.reshape(val_labels, (-1, 1))  # 同樣調整驗證數據形狀
+        self.batch_size = batch_size
+
+    def on_epoch_end(self, epoch, logs=None):
+        print(f"\n🌀 Epoch {epoch + 1} 結束，評估模型...")
+
+        # 使用 GradientTape 手動計算梯度
+        with tf.GradientTape() as tape:
+            predictions = self.model(self.train_data, training=True)
+            loss = self.model.compiled_loss(self.train_labels, predictions)
+
+        # 計算梯度
+        gradients = tape.gradient(loss, self.model.trainable_weights)
+        
+        # 輸出每層的梯度平均值
+        for weight, grad in zip(self.model.trainable_weights, gradients):
+            if grad is not None:
+                tf.print(f'🔍 Layer: {weight.name}, Gradient mean: {tf.reduce_mean(tf.abs(grad))}')
+            else:
+                tf.print(f'⚠️ Layer: {weight.name}, Gradient is None')
+
+        # 訓練集預測值和 loss
+        train_pred = self.model.predict(self.train_data, batch_size=self.batch_size)
+        train_loss = tf.keras.losses.binary_crossentropy(self.train_labels, train_pred)
+        print(f"📊 訓練集預測值範圍: {train_pred.min():.4f} - {train_pred.max():.4f}, 預測均值: {train_pred.mean():.4f}")
+        print(f"🧮 訓練集手動計算的 binary_crossentropy loss: {tf.reduce_mean(train_loss).numpy():.4f}")
+
+        # 驗證集預測值和 loss
+        val_pred = self.model.predict(self.val_data, batch_size=self.batch_size)
+        val_loss = tf.keras.losses.binary_crossentropy(self.val_labels, val_pred)
+        print(f"📊 驗證集預測值範圍: {val_pred.min():.4f} - {val_pred.max():.4f}, 預測均值: {val_pred.mean():.4f}")
+        print(f"🧮 驗證集手動計算的 binary_crossentropy loss: {tf.reduce_mean(val_loss).numpy():.4f}")
+        print(f"📝 Keras 記錄的 Loss: {logs['loss']:.4f}, Val Loss: {logs['val_loss']:.4f}")
+
+
 tensorboard_callback = tf.keras.callbacks.TensorBoard(log_dir='./history/', histogram_freq=1)
 # ✅ find all `.weights.h5` file
 checkpoint_files = glob.glob(os.path.join(MODEL_PATH, "weights_epoch_*.weights.h5"))
@@ -172,16 +201,27 @@ else:
     initial_epoch = 0  
 
 print("🔥🔥🔥 Start training！")
-
 history = prediction_model.fit(train_x, train_target,
                                validation_data=(val_x, val_target),
                                epochs=epochs, 
                                initial_epoch=initial_epoch,  
                                batch_size=batch_size,
-                               callbacks=callbacks)
+                               callbacks=[EpochDebugCallback(train_x,train_target,val_x,val_target)])
 
 # 取得模型對訓練集的預測結果
 y_pred_train = prediction_model.predict(train_x)
+y_pred_val = prediction_model.predict(val_x)
+
+# 查看模型預測值的分佈情況
+import matplotlib.pyplot as plt
+
+plt.hist(y_pred_train.flatten(), bins=50)
+plt.xlabel("預測值 (Predicted Probability)")
+plt.ylabel("頻率 (Frequency)")
+plt.title("模型初始預測值分佈")
+plt.show()
+
+# 如果預測值大部分集中在 0 或 1，會看到分佈圖兩端有很高的頻率
 
 # 建立 DataFrame，存儲預測與實際值
 rally_predictions_train = pd.DataFrame({