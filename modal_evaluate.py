f709011e2d5f02c3d604d141963a1eb2c168fde4
Yenhsin <123qqqselena@gmail.com>
2025-06-26 10:33:55 +0800

    transformer train and test

--- modal_evaluate.py
+++ modal_evaluate.py
@@ -47,17 +47,17 @@ indices_to_delete = [0, 3, 4, 5, 6, 7]
 test_shots = np.delete(test_shots, indices_to_delete, axis=2)
 
 # Model Hyperparameters
-batch_size = 32
+batch_size = 64
 cnn_kwargs = {'filters': 16, 'kernel_size': 3, 'kernel_regularizer': tf.keras.regularizers.l2(0.01)}
 transformer_kwargs = {
     'num_heads': 1,
-    'key_dim': 16,
-    'ff_dim': 16,
-    'inner_dim': 32
+    'key_dim': 32,
+    'ff_dim': 32,
+    'inner_dim': 64
 }
-optimizer = tf.keras.optimizers.Adam(learning_rate=0.0005, clipnorm=1.0)
+optimizer = tf.keras.optimizers.Adam(learning_rate=0.0002, clipnorm=1.0)
 MODEL_NAME = 'proposedModal'
-MODEL_PATH = "best_model_fold_1.keras"
+MODEL_PATH = "best_model_fold_3.keras"
 
 #input data
 test_x = [test_shots, test_shot_type, test_player_id,test_time_proportion,test_hit_area ,test_player_area, test_opponent_area,test_rallies,test_masks]
